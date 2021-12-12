19fe3be87076916ff7c279e6d92f978f3e7b869a
mohamed roshan <roshanpathayapurakkal@gmail.com>
2021-12-12 15:54:14 +0530

    intial commit tempchat

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import INPUT from './_Input';
+// mport contact512 from './public/logos/contact512px.png'i
+
 
 function App() {
+  const [cntrycd,setcode] = useState('91')
+  const [phno,setphno] = useState('')
+  let _link=`https://wa.me/${cntrycd}${phno}`
+  
+  const countrycode =(event)=>{
+    setcode(event.target.value)
+    console.log(event.target.value)
+  }
+  
+  const Phno = (event)=>{
+    setphno(event.target.value)
+    console.log(event.target.value)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='padding'>
+
+      <div className='align'>
+
+     <img src='logos/contact512px.png' alt="logo512" className='logo'/>
+      </div>
+    <div className='box'>
+
+        <INPUT countrycode={countrycode} phno={Phno} />
+        <div className='align'>
+          <a href={_link}>
+            <button>Chat</button>
+          </a>
+
+        </div>
+        <div className='align width'>
+
+          <p>your link: {_link}</p><br />
+        </div>
+        <div className='txtalign'>
+
+          <h3>note :</h3>
+          <p>Omit any zeroes, brackets, or dashes when adding the phone number.Add this website to home screen for better use.this is a proto type .Send me feed back
+            <br />Phone no : 8086041779
+          </p>
+        </div>
+      </div>
+      </div>
   );
 }
 
